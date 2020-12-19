e19e8c2e1c51cf49b51dcd63d4917bf75a9e1dd8
Sonal Pandey <pandeysonal25@gmail.com>
2020-12-18 20:59:59 -0800

    wrote all steps to git file

--- src/test/java/com/trainning/Demo_Git/Git.java
+++ src/test/java/com/trainning/Demo_Git/Git.java
@@ -1,5 +1,16 @@
 package com.trainning.Demo_Git;
 
 public class Git {
-//..
+/*
+	commands :-
+	-git init
+	-git status
+	-git remote add origin <Repo URL> (Copy URL from git Websit)
+	-git remote -v(To view the connection)
+	
+	create .gitignore file
+	-git commit -m "Message"
+	-git push origin master
+	
+	*/
 }
